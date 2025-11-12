013f72ccc6dbc0698c3d0de5813c13c8743ef09d
triadzyu <triadzyu@gmail.com>
2025-11-12 23:48:09 +0700

    Update instalsub.sh

--- instalsub.sh
+++ instalsub.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
-        if ! command -v bugscanner-go &> /dev/null; then
-        mkdir -p $HOME/go
+mkdir -p $HOME/go
+    if ! command -v bugscanner-go &> /dev/null; then
+        echo "[*] Menginstal bugscanner-go..."
         go install -v github.com/Toton-dhibar/bugscanner-go@latest
         chmod +x /usr/bin/bugscanner-go
-        fi
-        if ! command -v bugscanx-go &> /dev/null; then
+    fi
+    if ! command -v bugscanx-go &> /dev/null; then
+        echo "[*] Menginstal bugscanx-go..."
         go install -v github.com/Ayanrajpoot10/bugscanx-go@latest
         chmod +x /usr/bin/bugscanx-go
-        fi
-        if ! command -v subfinder &> /dev/null; then
+    fi
+    if ! command -v subfinder &> /dev/null; then
+        echo "[*] Menginstal subfinder..."
         go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
         chmod +x /usr/bin/subfinder
-        fi
-        
+    fi
